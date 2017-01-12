4a2864fb254d75a28496d62ac3908b515492d3d3
sapereaudete <sapereaudete@yahoo.com>
2017-01-12 18:29:05 +0100

    Create init.js

---
+++ init.js
@@ -0,0 +1,16 @@
+// init.js
+
+global["output"] = true,
+global["date"] = new Date(),
+global["showTopImage"] = false,
+global["log"] = function(a, b) {
+    b = b || "";
+    if (global.output === true) {
+        console.log(a + " " + b);
+    }
+}
+global.date = ("0" + global.date.getDate()).slice(-2) + "." + ("0" + (global.date.getMonth() + 1)).slice(-2) + "." + global.date.getFullYear();
+global.showTopImage = (readCookie("topImage") == global.date) ? false : true;
+if (global.showTopImage === true) {
+  document.getElementsByTagName("html")[0].className += " showTopImage";
+}
