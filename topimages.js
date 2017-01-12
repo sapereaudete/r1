784c5bb8fdc48a371844420d69552d5a7280be5f
sapereaudete <sapereaudete@yahoo.com>
2017-01-12 18:30:34 +0100

    Create topimages.js

---
+++ topimages.js
@@ -0,0 +1,29 @@
+/* topimages.js */
+
+global.topImages = [
+        {
+            "thema": "Contrails oder Chemtrails?",
+            "theme": "Contrails or Chemtrails?",
+            "url": "//2.bp.blogspot.com/-GcbW6flyZaE/WCWLEg4oIWI/AAAAAAAAAN0/nKt3XCSOxhQTFyE8iWp0uPzH8fWpEYx8wCK4B/s1600/chemtrails-PublicDomainPictures.net-HanaChramostova.jpg"
+        },
+        {
+            "thema": "Russischer Kampfjet? Wirklich?",
+            "theme": "Russian war jet? Really?",
+            "url": "//1.bp.blogspot.com/-9CTCwGzGIb8/WCWpMj0z9RI/AAAAAAAAAOE/dAaKIa0Rf9kS97cOWvFhA9-gT5l3vi1NgCK4B/s1600/waronterrororwaroftheterroristsusjetrussiancolorsuperstation95-v%253DsGeJCgC6q7M.png"
+        },
+        {
+            "thema": "Krieg gegen den Terror oder Globalisierung von Krieg und Terror?",
+            "theme": "War on terror or globalization of war and terror?",
+            "url": "//4.bp.blogspot.com/-o-ugKMAr_1w/WCW6fFl4K4I/AAAAAAAAAOU/ESDjgAWFZd8mi4AXdZ2mTO6UHePEwvH4ACK4B/s1600/TheGlobalizationOfWar-v%253Dim_HEX5ba6M.png"
+        },
+        {
+            "thema": "Bundesstaatsangehörige oder Reichsbürger?",
+            "theme": "Member of a federal state of the German Reich or member of the Third Reich?",
+            "url": "//1.bp.blogspot.com/-ta86mkriVzg/WDMff4dsoDI/AAAAAAAAAQ8/rG4mzUwYOOAeLzT3uWgwCfugiu9ahrU8gCK4B/s1600/Deutsches_Reich_%25281871-1918%2529-de-angepasst.png"
+        },
+        {
+            "thema": "Der wahre Grund für ihre vierte Amtszeit?",
+            "theme": "The real reason for her fourth term?",
+            "url": "//2.bp.blogspot.com/-sUUeXdlrcBw/WDL5nSdoXeI/AAAAAAAAAQs/as_gOrJwKSE6O0eaASudPa9PlOOK4XdewCK4B/s1600/merkel-deagel-2025.png"
+        }
+]
