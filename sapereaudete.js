698762a279d41f13bf7272df092bfa9c410a7078
sapereaudete <sapereaudete@yahoo.com>
2017-03-25 13:56:20 +0100

    Update sapereaudete.js

--- sapereaudete.js
+++ sapereaudete.js
@@ -208,3 +208,21 @@ if ($("#postLizenz")) {
     //document.querySelector('#desktopVersion').href 
   }
 }) ();
+
+// wg. einstieg.html
+// http://stackoverflow.com/questions/17534661/make-anchor-link-go-some-pixels-above-where-its-linked-to
+// The function actually applying the offset
+function offsetAnchor() {
+    if(location.hash.length !== 0) {
+        window.scrollTo(window.scrollX, window.scrollY - 125);
+    }
+}
+
+// This will capture hash changes while on the page
+window.addEventListener("hashchange", offsetAnchor);
+
+// This is here so that when you enter the page with a hash,
+// it can provide the offset in that case too. Having a timeout
+// seems necessary to allow the browser to jump to the anchor first.
+// The delay of 1 is arbitrary and may not always work right (although it did in my testing).
+window.setTimeout(offsetAnchor, 1);
